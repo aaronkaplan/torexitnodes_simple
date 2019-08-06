310eacde9da9f0260c33d993a0848019df62195c
aaronkaplan <aaron@lo-res.org>
2019-08-06 19:01:36 +0200

    layout

--- www/tor/tornode.py
+++ www/tor/tornode.py
@@ -122,7 +122,7 @@ api.add_resource(IP, '/ip/<string:ip>')
 def show_entries():
     cur = g.psql.cursor()
     try:
-        res = cur.execute('select ip,exit_address_ts,nodetype.type from node,nodetype,source WHERE id_nodetype=nodetype.id order by ip asc limit 1000;')
+        res = cur.execute('select ip,exit_address_ts,nodetype.type from node,nodetype WHERE id_nodetype=nodetype.id order by ip asc limit 1000;')
         if (DEBUG):
             print(cur, file=sys.stderr)
     except psycopg2.Error as e:
